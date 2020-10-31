611bc6d1caa251f1a7220223282c6a8ccf8c762a
DmitryKramich <55790720+DmitryKramich@users.noreply.github.com>
2020-10-31 05:27:29 +0300

    fix

--- install_files/check.sh
+++ install_files/check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ourVer=myvariable
-Ver=\$(cat /usr/local/tomcat/webapps/helloworld-ws/index.html | grep ver | awk -F "ver:" {'print \$2'} | awk -F "<" {'print \$1'} | sed '/^$/d')
-if [ "\$Ver" -eq "\$ourVer" ]; then
+Ver=$(cat /usr/local/tomcat/webapps/helloworld-ws/index.html | grep ver | awk -F "ver:" {'print $2'} | awk -F "<" {'print $1'} | sed '/^$/d')
+if [ "$Ver" -eq "$ourVer" ]; then
   exit 0
 else
   exit 1
