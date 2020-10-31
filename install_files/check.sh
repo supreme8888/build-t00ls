a3248cb2006a94e7f3c3c904b8086ff0d59f7ee2
DmitryKramich <55790720+DmitryKramich@users.noreply.github.com>
2020-10-31 05:04:03 +0300

    fix again

--- install_files/check.sh
+++ install_files/check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ourVer="${BUILD_NUMBER}"
+ourVer=myvariable
 Ver=\$(cat /usr/local/tomcat/webapps/helloworld-ws/index.html | grep ver | awk -F "ver:" {'print \$2'} | awk -F "<" {'print \$1'} | sed '/^$/d')
 if [ "\$Ver" -eq "\$ourVer" ]; then
   exit 0
